5e2e40e9ce5a2a3378b4b9a73f558fce36536c82
Oğuz Özcan <centrual@users.noreply.github.com>
2018-07-05 20:14:36 +0300

    Update UdpSocketsModule.java

--- android/src/main/java/com/tradle/react/UdpSocketsModule.java
+++ android/src/main/java/com/tradle/react/UdpSocketsModule.java
@@ -30,7 +30,6 @@ public final class UdpSocketsModule implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
